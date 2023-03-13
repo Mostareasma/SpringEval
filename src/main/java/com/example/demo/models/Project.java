b34860bae8195a2184e24013d6331f3304be4ece
Mostareasma <56792517+Mostareasma@users.noreply.github.com>
2023-03-13 09:05:11 +0100

    first commit

---
+++ src/main/java/com/example/demo/models/Project.java
@@ -0,0 +1,17 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String title;
+    String description;
+
+}
